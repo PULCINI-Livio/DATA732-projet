6ac83b6a718c5e8383f5be699deadcee05320fe6
corailllum <160240585+corailllum@users.noreply.github.com>
2024-12-02 16:12:47 +0100

    cartebien

--- carteIntercative.py
+++ carteIntercative.py
@@ -18,7 +18,7 @@ df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
 df_pays_capitale = df[['NOM', 'CAPITALE','CODE']]
 
 #Afficher les premières lignes pour vérification
-print(df_pays_capitale)
+
 def trouver_pays_par_capitale(capitale):
     try:
         pays = df.loc[df_pays_capitale['Capitale'] == capitale, 'Pays'].iloc[0]
@@ -39,7 +39,7 @@ df_Occurence=pd.DataFrame({'pays': [],'Occurence': [],'code_iso':[]})
          if lieu in df['NOM'].unique():
                 if is_a_state(lieu)  :
 """
-#on verra les capitale avec livio
+
 def traitement():
     """
     fonction qui permet de traité les donnée du json pour pouvoir compté le nombre d'occurence 
@@ -51,45 +51,80 @@ def traitement():
             df_Occurence.loc[len(df_Occurence)]=[lieu,data["metadata"]["all"]["loc"][lieu], df.loc[df['NOM'] == lieu, 'CODE'].iloc[0]]
         
     return df_Occurence
-print(traitement())
 
+traitement()
 
 
 # Créer la carte à bulles
-fig = px.scatter_geo(df_Occurence, 
-                     locations="code_iso", 
-                     size="Occurence", # Définir la taille des bulles selon la population 
-                       hover_name="pays",# Afficher le nom du pays au survol
-                     size_max=30,  # Taille maximale des bulles
-                     projection="natural earth",  # Projection de la carte
-                     title="Carte à Bulles des Pays par Population")
+#fig = px.scatter_geo(df_Occurence, 
+#                     locations="code_iso", 
+#                     size="Occurence", # Définir la taille des bulles selon la population 
+#                       hover_name="pays",# Afficher le nom du pays au survol
+#                     size_max=30,  # Taille maximale des bulles
+#                     projection="natural earth",  # Projection de la carte
+#                     title="Carte à Bulles des Pays par Population")
 
 # Afficher la carte
-fig.show()
+#fig.show()
 
 
-# rajouté le calcule avec les capitale 
-# rajouté les code 3 lettre dans le df_Occurence 
-# finir de faire la carte 
-# rajouté les couleur par taille de nombre d'occurence 
+# Exemple de DataFrame
+data = {
+    "pays": ["France", "Algérie", "Russie"],
+    "Occurence": [2663, 1287, 8326],
+    "code_iso": ["FRA", "DZA", "RUS"]
+}
+df = pd.DataFrame(data)
 
+# Initialisation de l'application Dash
+app = Dash(__name__)
 
+app.layout = html.Div([
+    html.H4('Carte des occurrences par pays'),
+    dcc.Dropdown(
+        id="dropdown",
+        options=[{"label": pays, "value": code_iso} for pays, code_iso in zip(df_Occurence["pays"], df_Occurence["code_iso"])],
+        value="FRA",
+        clearable=False,
+    ),
+    dcc.Graph(id="map"),
+])
 
+@app.callback(
+    Output("map", "figure"),
+    [Input("dropdown", "value")]
+)
+def update_map(selected_country):
+    # Créer une carte choroplèthe
+    fig = px.choropleth(
+        df_Occurence,
+        locations="code_iso",
+        color="Occurence",
+        hover_name="pays",
+        color_continuous_scale=['#C1C5FD','#929AFC','#636EFA','#32377D']
+    )
+    # Mettre en évidence le pays sélectionné
+    fig.update_traces(
+        marker_line_width=1,
+        marker_line_color="black"
+    )
+    fig.update_layout(
+        title="Occurrences par pays",
+        geo=dict(showframe=False, showcoastlines=True),
+    )
+    return fig
 
+@app.callback(
+    Output("dropdown", "value"),
+    [Input("map", "clickData")]
+)
+def update_dropdown_from_map(click_data):
+    if click_data is not None:
+        return click_data["points"][0]["location"]  # Renvoie le code_iso du pays cliqué
+    return "FRA"  # Valeur par défaut
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    app.run_server(debug=True)
 
 
 
