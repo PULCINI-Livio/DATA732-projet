151fe01127634f67122f82b12d9098c4c3428ae9
PULCINI-Livio <pulcinilivio04@gmail.com>
2024-12-05 10:34:14 +0100

    Update fusion_dash.py

--- fusion_dash.py
+++ fusion_dash.py
@@ -10,36 +10,38 @@ from link_chart import *
 
 #Chemin des fichiers
 file_path = 'data/liste-197-etats-2020.csv'
-
 file_name = "data/fr.sputniknews.africa--20220630--20230630.json"
 
 #Charger le fichier CSV en utilisant l'encodage ISO-8859-1 et le point-virgule comme délimiteur
 df = pd.read_csv(file_path, encoding='ISO-8859-1', delimiter=';')
-#Sélectionner seulement les colonnes "NOM" et "CAPITALE"
+
+#Sélectionner seulement les colonnes "NOM", "CAPITALE" et "CODE"
 df_pays_capitale_code = df[['NOM', 'CAPITALE', 'CODE']]
 
 liste_pays = df_pays_capitale["NOM"]
 liste_pays = [unidecode(pays) for pays in liste_pays]
 
-#Afficher les premières lignes pour vérification
-#print(df_pays_capitale)
-
-
+# Initialisation des dataframes de chaque graphique 
+df_map = traitement(file_name)
+df_bar = cpt_link_btw_states(file_name)
 
 # Initialisation de l'application Dash
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H4('Carte'),
+    
     dcc.Dropdown(
         id="dropdown",
         options=liste_pays,
         value="Russie",
         clearable=False,
     ),
+    html.H4('Carte'),
     dcc.Graph(id="map"),
+
     html.H4("Graphique des liens"),
     dcc.Graph(id="bar-chart"),
+
     html.H4("Graphique des occurences"),
     dcc.Graph(id="line-chart"),
 ])
@@ -51,9 +53,9 @@ app.layout = html.Div([
 )
 def update_map(selected_country):
     # Créer une carte choroplèthe
-    df = traitement()
+    ## df_map = traitement()
     fig = px.choropleth(
-        df,
+        df_map,
         locations="code_iso",
         color="Occurence",
         hover_name="pays",
@@ -86,37 +88,36 @@ def update_dropdown_from_map(click_data):
     [Input("dropdown", "value")]
 )
 def update_bar_chart(pays):
-    df = cpt_link_btw_states(file_name)
-    df = df
-    mask = (df["Pays1"] == pays) | (df["Pays2"] == pays)
+    ## df_bar = cpt_link_btw_states(file_name)
+    mask = (df_bar["Pays1"] == pays) | (df_bar["Pays2"] == pays)
     #inversion des pays pour ne mettre {pays} que dans la première colonne
-    for index, row in df.iterrows():
+    for index, row in df_bar.iterrows():
         if row['Pays2'] == pays:
             # Échanger les valeurs de Pays1 et Pays2
-            df.at[index, 'Pays1'], df.at[index, 'Pays2'] = df.at[index, 'Pays2'], df.at[index, 'Pays1']
-    #df = df.head(10)
-    fig = px.bar(df[mask], x="Pays2", y="NbLink")
+            df_bar.at[index, 'Pays1'], df_bar.at[index, 'Pays2'] = df_bar.at[index, 'Pays2'], df_bar.at[index, 'Pays1']
+    #df_bar = df_bar.head(10)
+    fig = px.bar(df_bar[mask], x="Pays2", y="NbLink")
     return fig
 
 
-# Callback pour mettre à jour le bar chart des occurences
+# Callback pour mettre à jour le line chart des occurences
 @app.callback(
     Output("line-chart", "figure"),
     [Input("dropdown", "value")]
 )
 def update_line_chart(pays):
     # Appeler creer_figure pour obtenir les données
-    df = creer_figure(pays)
+    df_line = creer_figure(pays)
     
-    # Vérifier si df est vide ou None
-    if df is None or df.empty:
+    # Vérifier si df_line est vide ou None
+    if df_line is None or df_line.empty:
         print(f"Aucune donnée à afficher pour le pays : {pays}")
         return px.line(title="Aucune donnée disponible")
     
     # Appliquer le masque pour filtrer les données du pays
-    mask = df["Pays"] == pays
+    mask = df_line["Pays"] == pays
     fig = px.line(
-        df[mask], 
+        df_line[mask], 
         x="Date_Pays", 
         y="Nombre d'articles", 
         title=f"Nombre d'articles pour {pays}", 
